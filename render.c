d83858ee0f5f7b4938207275fb91430638d80230
TiggyWiggler <craigfreakmoore@aol.com>
2021-07-17 21:23:08 +0100

    completed the ability to add text to the hosts and changed the implementation of the drawing and writting methods to act upon an array of chars rather than three 2D arrays or R, G, and B channels. This allows me to move the bitmap array around as a single pointer to the array of chars rather than creating pointer to pointers for three interconnected arrays

--- render.c
+++ render.c
@@ -11,6 +11,203 @@
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 
+/**
+ * Set a single pixel in a bitmap
+ * @param [in]  d       bitmap data. assumes three channel (RBG)
+ * @param [in]  w       bitmap width
+ * @param [in]  h       bitmap height
+ * @param [in]  x       x position of the set pixel (top left is 0,0)
+ * @param [in]  y       y position of the set pixel (top left is 0,0)
+ * @param [in]  r       byte representation of the red channel of the pixel to be set.
+ * @param [in]  g       byte representation of the green channel of the pixel to be set.
+ * @param [in]  b       byte representation of the blue channel of the pixel to be set.
+ * */
+void setPixel(char *d, unsigned int w, unsigned int h, unsigned int x, unsigned int y, char r, char g, char b)
+{
+    int bpp = 3; // bytes per pixel.
+    int pos = y * w * bpp;
+    pos += x * bpp;
+    d[pos++] = r;
+    d[pos++] = g;
+    d[pos] = b;
+}
+
+/**
+ * Set a line in a bitmap
+ * @param [in]  d       bitmap data. assumes three channel (RBG)
+ * @param [in]  w       bitmap width
+ * @param [in]  h       bitmap height
+ * @param [in]  x0      x position of the start of the line
+ * @param [in]  y0      y position of the start of the line
+ * @param [in]  x1      x position of the end of the line
+ * @param [in]  y1      y position of the end of the line
+ * @param [in]  r       byte representation of the red channel of the line
+ * @param [in]  g       byte representation of the green channel of the line
+ * @param [in]  b       byte representation of the blue channel of the line
+ * */
+void drawLine(char *d, unsigned int w, unsigned int h, unsigned int x0, unsigned int y0, unsigned int x1, unsigned int y1, char r, char g, char b)
+{
+    // Both ends of the line have been resolved. Draw them on the bitmap
+    // https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
+    int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+    int err = (dx > dy ? dx : -dy) / 2, e2;
+
+    for (;;)
+    {
+        setPixel(d, w, h, x0, y0, r, g, b);
+
+        if (x0 == x1 && y0 == y1)
+            break;
+        e2 = err;
+        if (e2 > -dx)
+        {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dy)
+        {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+/**
+ * Write text into an image bitmap
+ * @param [in]  text    text to be written (null terminated)
+ * @param [in]  d       bitmap data. assumes three channel (RBG)
+ * @param [in]  w       bitmap width
+ * @param [in]  h       bitmap height
+ * @param [in]  fs      font size in pixels
+ * @param [in]  x       x position of the text reference box. Writting from left to right this would be the top left of the text box. Centre aligned, this would be the top centre of the text box.
+ * @param [in]  y       y position of the text reference box. Writting from left to right this would be the top left of the text box. Centre aligned, this would be the top centre of the text box.
+ * @param [in]  maxw    max width of the text box.
+ * */
+void writeText(char *text, char *d, unsigned int w, unsigned int h, unsigned int fs, unsigned int x, unsigned int y, int maxw)
+{
+
+    // Write some text for each host.
+    // See example code for Hello World in stb_truetype.h
+    const int bpp = 3; // Bytes per pixel.
+    int x0, y0, x1, y1;
+    int i, j;
+    float scale, xpos = 2; // leave a little padding in case the character extends left
+    int ascent, baseline, ch = 0, advance, lsb;
+
+    // Load the font.
+    char *buffer = malloc(7 << 18); // file's 108.2kB, this will be big enough.
+    FILE *f = fopen("fonts/LiberationMono-Regular.ttf", "rb");
+    fread(buffer, 1, 1000000, f);
+    fclose(f);
+    stbtt_fontinfo font;
+    stbtt_InitFont(&font, buffer, 0);
+
+    // Retrieve variables that are constant for the entire font set
+    scale = stbtt_ScaleForPixelHeight(&font, fs);
+    stbtt_GetFontVMetrics(&font, &ascent, 0, 0);
+    baseline = (int)(ascent * scale);
+
+    // Get details for the ellipse (...) character which will be used if the requested text will not fit in the maximum width.
+    stbtt_GetCodepointHMetrics(&font, 0x2026, &advance, &lsb);
+    int ew = ceil(advance * scale); // Elipse width
+
+    if (x + maxw > w)
+        maxw = w - x;
+
+    if (ew > maxw)
+    {
+        free(buffer);
+        return;
+    }
+
+    while (text[ch])
+    {
+        int bmpW, bmpH, bmpXOff, bmpYOff;
+        int last = 0;
+        stbtt_GetCodepointHMetrics(&font, text[ch], &advance, &lsb);
+        unsigned char *glyph;
+
+        // There must be enough room for an ellipses unless we are adding the last character and there is enough room for that instead.
+        if (text[ch + 1] && xpos + (advance * scale) + ew > maxw)
+        {
+            // If we insert the current character then there is not enough space for the elipses after it.
+            glyph = stbtt_GetCodepointBitmap(&font, scale, scale, 0x2026, &bmpW, &bmpH, &bmpXOff, &bmpYOff);
+            last = 1; // make this the last character.
+        }
+        else if (xpos + (advance * scale) > maxw)
+        {
+            // Last character, but not enough room for it, we have to add an elipses
+            glyph = stbtt_GetCodepointBitmap(&font, scale, scale, 0x2026, &bmpW, &bmpH, &bmpXOff, &bmpYOff);
+        }
+        else
+        {
+            // plenty of room. add the character.
+            glyph = stbtt_GetCodepointBitmap(&font, scale, scale, text[ch], &bmpW, &bmpH, &bmpXOff, &bmpYOff);
+        }
+
+        int pos = 0;
+        for (i = 0; i < bmpH; i++)
+        {
+            int yAdj = (i + baseline + bmpYOff + y); // Y position of the bmp adjusted against the baseline
+            if (yAdj < 0 || yAdj > h)
+                break;                                  // outside of confines of bitmap on y axis.
+            pos = ((yAdj * w) + floor(xpos) + x) * bpp; // (yAdj * w) is used to work out which row we are on. floor(xpos) gives us the x position in the desired row.
+            for (j = 0; j < bmpW; j++)
+            {
+                d[pos++] = ~glyph[(i * bmpW) + j];
+                d[pos++] = ~glyph[(i * bmpW) + j];
+                d[pos++] = ~glyph[(i * bmpW) + j];
+            }
+        }
+        if (last == 1)
+            break;
+        xpos += (advance * scale);
+        if (text[ch + 1])
+            xpos += scale * stbtt_GetCodepointKernAdvance(&font, text[ch], text[ch + 1]);
+        ++ch;
+        free(glyph);
+    }
+    free(buffer);
+}
+
+/**
+ * Draw a single host into an image bitmap
+ * @param [in]  host    host to be drawn
+ * @param [in]  d       bitmap data. assumes three channel (RBG)
+ * @param [in]  w       bitmap width
+ * @param [in]  h       bitmap height
+ * @param [in]  x       x position of the text reference box. Top left corner typically, but I may make a 'centre align' option at a later date.
+ * @param [in]  y       y position of the text reference box. Top left corner typically, but I may make a 'centre align' option at a later date.
+ * @param [in]  maxw    max width of the drawn host
+ * @param [in]  maxh    max height of the drawn host
+ * */
+void drawHost(struct host *host, char *d, unsigned int w, unsigned int h, unsigned int x, unsigned int y, int maxw, int maxh)
+{
+    int sprx, spry, sprn; // Sprite x (width), y (height), n ('comp' which is 3 for RGB, 4 for RGBA (Specific to stb library)).
+    unsigned char *data = stbi_load("images/XC206-2SFP_96.jpg", &sprx, &spry, &sprn, 0);
+    int bpp = 3;                    // bytes per pixes
+    int stride = (w - sprx) * bpp;  // the stride in data required to go from the end of data on one row to the start of the data on another row.
+    int srcPos = 0, tarPos, rowPos; // data position indicators (relative pointers);
+    int i, j;
+
+    tarPos = y * w * bpp;
+    tarPos += x * bpp;
+
+    while (srcPos < sprx * spry * bpp)
+    {
+        rowPos = 0;
+        while (rowPos < (sprx * bpp))
+        {
+            d[tarPos++] = data[srcPos++];
+            rowPos++;
+        }
+
+        tarPos += stride;
+    }
+    stbi_image_free(data);
+}
+
 void renderHL(struct hostLink *hl, struct padding *pads)
 {
     // Render a host link collection to bitmap file.
@@ -45,25 +242,32 @@ void renderHL(struct hostLink *hl, struct padding *pads)
             blue[i][j] = 255;
         }
 
-    // Draw the hosts.
-    int pos = 0;          // pointer location
-    int sprx, spry, sprn; // Sprite x (width), y (height), n ('comp' which is 3 for RGB, 4 for RGBA (Specific to stb library)).
-    unsigned char *data = stbi_load("images/XC206-2SFP_96.jpg", &sprx, &spry, &sprn, 0);
+    
 
+    unsigned char *d = malloc((sizeof d * (h * w)));
+
+    // write to character array
+    int pos = 0;
+
+    for (j = 0; j < h; j++)
+        for (i = 0; i < w; i++)
+        {
+            d[pos++] = (unsigned char)red[i][j];
+            d[pos++] = (unsigned char)green[i][j];
+            d[pos++] = (unsigned char)blue[i][j];
+        }
+
+    // Output the host data.
     for (i = 0; i < hl->hosts.count; i++)
     {
-        pos = 0; // reset pointer
         host = &hl->hosts.hosts[i];
-        for (k = host->yPos; (k < host->yPos + host->h) && (k - host->yPos < spry); k++)
-            for (j = host->xPos; (j < host->xPos + host->w) && (j - host->xPos < sprx); j++)
-            {
-                red[j][k] = data[pos++];
-                green[j][k] = data[pos++];
-                blue[j][k] = data[pos++];
-            }
-    }
 
-    stbi_image_free(data);
+        // Draw the host itself
+        drawHost(host, d, w, h, host->xPos, host->yPos, host->w, host->h);
+
+        // Write the text for the hosts.
+        writeText(host->name, d, w, h, 15, host->xPos, host->yPos + host->h - 15, host->w);
+    }
 
     // Draw lines between hosts.
     int x0, y0, x1, y1; // Start and end x,y positions for the line.
@@ -95,119 +299,37 @@ void renderHL(struct hostLink *hl, struct padding *pads)
         }
         if (n0 > 0 && n1 > 0)
         {
-            // Both ends of the line have been resolved. Draw them on the bitmap
-            // https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
-            int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-            int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-            int err = (dx > dy ? dx : -dy) / 2, e2;
+            drawLine(d, w, h, x0, y0, x1, y1, 0, 0, 0);
+            /*
+                // Both ends of the line have been resolved. Draw them on the bitmap
+                // https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
+                int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+                int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+                int err = (dx > dy ? dx : -dy) / 2, e2;
 
-            for (;;)
-            {
-                //setPixel(x0, y0);
-                red[x0][y0] = 0;
-                green[x0][y0] = 0;
-                blue[x0][y0] = 0;
+                for (;;)
+                {
+                    //setPixel(x0, y0);
+                    red[x0][y0] = 0;
+                    green[x0][y0] = 0;
+                    blue[x0][y0] = 0;
 
-                if (x0 == x1 && y0 == y1)
-                    break;
-                e2 = err;
-                if (e2 > -dx)
-                {
-                    err -= dy;
-                    x0 += sx;
-                }
-                if (e2 < dy)
-                {
-                    err += dx;
-                    y0 += sy;
-                }
-            }
+                    if (x0 == x1 && y0 == y1)
+                        break;
+                    e2 = err;
+                    if (e2 > -dx)
+                    {
+                        err -= dy;
+                        x0 += sx;
+                    }
+                    if (e2 < dy)
+                    {
+                        err += dx;
+                        y0 += sy;
+                    }
+                }*/
         }
     }
-
-    unsigned char *d = malloc((sizeof d * (h * w)));
-
-    // write to character array
-    pos = 0;
-
-    for (j = 0; j < h; j++)
-        for (i = 0; i < w; i++)
-        {
-            d[pos++] = (unsigned char)red[i][j];
-            d[pos++] = (unsigned char)green[i][j];
-            d[pos++] = (unsigned char)blue[i][j];
-        }
-
-    // Write some text for each host.
-    // See example code for Hello World in stb_truetype.h
-    //int x0, y0, x1, y1;
-    float scale, xpos = 2; // leave a little padding in case the character extends left
-    int ascent, baseline, ch = 0, advance, lsb;
-    char *text = "1234";
-    char *buffer = malloc(7 << 14); // file's 108.2kB, this will clear it.
-    fread(buffer, 1, 1000000, fopen("fonts/LiberationMono-Regular.ttf", "rb"));
-
-    stbtt_fontinfo font;
-    stbtt_InitFont(&font, buffer, 0);
-
-    scale = stbtt_ScaleForPixelHeight(&font, 20);
-    stbtt_GetFontVMetrics(&font, &ascent, 0, 0);
-    baseline = (int)(ascent * scale);
-
-    int charStart, stride;
-
-    while (text[ch])
-    {
-        int bmpW, bmpH, bmpXOff, bmpYOff;
-        unsigned char *glyph = stbtt_GetCodepointBitmap(&font, scale, scale, text[ch], &bmpW, &bmpH, &bmpXOff, &bmpYOff);
-        int pos = 0;
-        for (i = 0; i < bmpH; i++)
-        {
-            pos = ((i * w) + xpos) * 3;
-            for (j = 0; j < bmpW; j++)
-            {
-                d[pos++] = ~glyph[(i * bmpW) + j];
-                d[pos++] = ~glyph[(i * bmpW) + j];
-                d[pos++] = ~glyph[(i * bmpW) + j];
-            }
-        }
-        stbtt_GetCodepointHMetrics(&font, text[ch], &advance, &lsb);
-        //xpos += bmpW;     // If using the advance variable you do not need to manually account for the glyph width.
-        xpos += (advance * scale);
-        if (text[ch + 1])
-            xpos += scale * stbtt_GetCodepointKernAdvance(&font, text[ch], text[ch + 1]);
-        ++ch;
-
-        /*float x_shift = xpos - (float)floor(xpos); // Not quite sure what this is doing. Looks like it is trying to kill rounding error.
-        
-        stbtt_GetCodepointBitmapBox(&font, text[ch], scale, scale, &x0, &y0, &x1, &y1);
-        int outw, outh;
-        outw = x1 - x0;
-        outh = y1 - y0;
-        char glyph[outh][outw];
-        stbtt_MakeCodepointBitmap(&font, &glyph[baseline + y0][0], outw, outh, 0, scale, scale, text[ch]);
-
-        // Write glyph to bitmap.
-        int bmppos;
-        for (j = 0; j < y1 - y0; j++)
-        {
-            bmppos = (xpos + (j * w)) * 3;
-            for (i = 0; i < x1 - x0; i++)
-            {
-                d[bmppos++] = glyph[j][i];
-                d[bmppos++] = glyph[j][i];
-                d[bmppos++] = glyph[j][i];
-            }
-        }
-
-        xpos += (advance * scale);
-        if (text[ch + 1])
-            xpos += scale * stbtt_GetCodepointKernAdvance(&font, text[ch], text[ch + 1]);
-
-        ++ch;*/
-    }
-
-    free(buffer);
 
     int bpp = 3; // Bytes per pixel
     stbi_write_bmp(name, w, h, bpp, d);
